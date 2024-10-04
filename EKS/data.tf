46af97f248ca0e745e2e8addc6cea841643a7c3b
srikanthg <skanth306@gmail.com>
2024-10-04 17:16:28 +0530

    AWS-EKS-alb-Helm-ArgoCD-Prometheus-Grafana

--- EKS/data.tf
+++ EKS/data.tf
@@ -25,7 +25,7 @@ data "tls_certificate" "eks-certificate" {
   url = aws_eks_cluster.eks[0].identity[0].oidc[0].issuer
 }
 
-data "aws_eks_cluster" "eks-cluster" {
+data "aws_eks_cluster" "eks" {
   name = var.cluster_name
 }
 
